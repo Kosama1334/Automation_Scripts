677c579f4b507f7f38896fb9e48bf64012b4262f
Kosama1334 <75474064+Kosama1334@users.noreply.github.com>
2025-12-21 02:18:37 +0530

    Update ec2_manager.py - Added functions for start/stop and argument parsing block

--- scripts/AWS_using_boto3/ec2_manager.py
+++ scripts/AWS_using_boto3/ec2_manager.py
@@ -1,6 +1,7 @@
 # Basic script to manage EC2 instances using CLI
 
 import boto3
+import agrparse
 
 ec2 = boto3.client("ec2")
 
@@ -13,8 +14,54 @@ def listEC2_Instances():
       instance_id = instance["InstanceId"]
       state = instance["State"]["Name"]
       
-      print(f"Instance_ID: {instance_id}\nInstance_State: {state}")
+      print(f"Instance_ID: {instance_id}\nInstance_State: {state}\n\n")
+
+#function to get instance_state
+def get_instance_state(instance_id):
+  resp = ec2.describe_instances()
+  return (resp["Reservations"][0]["Instances"][0]["State"]["Name"])
+
+#function to start a stopped instance
+def start_stopped_instance(instance_id):
+  state = get_instance_state(instance_id)
+  if state == "running":
+    print(f"Instance with ID: {instance_id} already running\n")
+    return
+
+  ec2.start_instances(Instance_Ids=[instance_id])
+  print(f"Instance with ID: {instance_id} started!\n")
+
+def stop_instance(instance_id):
+  state = get_instance_state(instance_id)
+  if state == "stopped":
+    print(f"Instance with ID: {instance_id} already stopped\n")
+    return
+
+  ec2.stop_instances(InstanceIds=[instance_id])
+  print(f"Instance with ID: {instance_id} stopped!\n")
+
 
 
 if __name__ == "__main__":
-  listEC2_Instances()
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("--list", action="store_true")
+  parser.add_argument("--start", action="store_true")
+  parser.add_argument("--stop", action="store_true")
+  parser.add_argument("--id")
+  
+  if args.list:
+    listEC2Instances()
+    
+  elif args.start:
+    if args.id:
+      start_stopped_instance(args.id)
+    else:
+      print("[ERROR] Please mention the Instance ID\n")
+
+  elif args.stop:
+    if args.id:
+      stop_instance(args.id)
+    else:
+      print("[ERROR] Please mention the Instance ID\n")
