3f17f6b4abaa0db06d70aa8ea69e8ebfc823a4c8
Kosama1334 <75474064+Kosama1334@users.noreply.github.com>
2025-12-21 19:54:53 +0530

    Add an uplaod_file function to upload an object to S3

--- scripts/s3_using_boto3/main.py
+++ scripts/s3_using_boto3/main.py
@@ -1,1 +1,33 @@
+import boto3
+import os
+import argparse
 
+
+s3 = boto3.client("s3")
+
+#function to upload file to s3
+def upload_file(bucket, file_path):
+
+    if os.path.exists(file_path):
+        key = os.path.basename(file_path)
+
+        s3.upload_file(file_path, bucket, key)
+        print(f"file {key} uploaded to bucket {bucket}!\n#####OK#####\n")
+
+        return
+
+    else:
+        print(f"[ERROR] File not found!\n Please Check the path specified\n#####ERROR#####\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--bucket", help="Bucket name")
+    parser.add_argument("--upload", action="store_true")
+    parser.add_argument("--file", help="File to be uploaded")
+
+    agrs = parser.parse_args()
+
+    if args.upload:
+        upload_file(args.bucket, args.file)
